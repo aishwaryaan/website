c4b107ea199eb0290dba0ee04ba4f378d5c4f22d
Aishwarya <aishnarw@gmail.com>
2026-05-14 15:07:26 +0530

    updated footer and added fash project for des

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,13 +4,17 @@ import { MdEmail } from "react-icons/md";
 export default function Footer() {
   return (
     <div className="footer">
+      <p>Let's connect!</p>
+      <br></br>
       <div className="social-links">
         <a href="www.linkedin.com/in/aishwarya-a-narwadkar-11199a192" target="_blank" rel="noopener noreferrer" className="social-icon"><FaLinkedin /></a>
         <a href="aishnarw@gmail.com" className="social-icon"><MdEmail /></a>
         <a href="https://github.com/aishwaryaan" target="_blank" rel="noopener noreferrer" className="social-icon"><FaGithub /></a>
         <a href="https://www.instagram.com/bas.kar.aish?igsh=eDZibnU4b2N0OTZk&utm_source=qr" target="_blank" rel="noopener noreferrer" className="social-icon"><FaInstagram /></a>
       </div>
-      <p>&copy; {new Date().getFullYear()} Aishwarya's Portfolio.</p>
+      <br></br>
+      <p style={{ color: "grey" }}>&copy; {new Date().getFullYear()} Aishwarya's Portfolio.</p>
+      <p style={{ fontSize: "0.9rem" }}>Made with ❤️ and React</p>
     </div>
   );
 }
